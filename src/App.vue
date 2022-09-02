81519f681238c1fc86a64edcab6023d2a1090d2f
Cxx0822 <1556464090@qq.com>
2022-09-02 22:00:35 +0800

    页面重新排版设计

--- src/App.vue
+++ src/App.vue
@@ -5,140 +5,149 @@
     </div>
 
     <div class="workTime-div">
-      <div class="workTime-table">
-        <el-table
-          :data="workingTimeInfo.workTime"
-          border
-          style="width: 1100px"
-          @cell-dblclick="tabClick"
-          :row-class-name="tableRowClassName"
-          :cell-class-name="tableCellClassName"
-        >
-          <el-table-column align="center" label="序号" width="100">
-            <template #default="scope">
-              {{ scope.row.index + 1 }}
-            </template>
-          </el-table-column>
-          <el-table-column prop="id" align="center" label="工号" width="200" />
-          <el-table-column prop="name" align="center" label="姓名" width="200" />
-          <el-table-column align="center" label="日期" width="200">
-            <template #default="scope">
-              <div v-if="scope.row.index === workingTimeInfo.clickRow && scope.column.index === workingTimeInfo.clickCell">
-                <el-input
-                  ref="dateRef"
-                  v-model="scope.row.date"
-                  maxlength="300"
-                  size="small"
-                  @keyup.enter="updateData"
-                  @blur="updateData"
-                />
-              </div>
-              <div v-else>{{ scope.row.date }}</div>
-            </template>
-          </el-table-column>
-          <el-table-column align="center" label="上班时间" width="200">
-            <template #default="scope">
-              <div v-if="scope.row.index === workingTimeInfo.clickRow && scope.column.index === workingTimeInfo.clickCell">
-                <el-input
-                  ref="startTimeRef"
-                  v-model="scope.row.startTime"
-                  maxlength="300"
-                  size="small"
-                  @keyup.enter="updateData"
-                  @blur="updateData"
-                />
-              </div>
-              <div v-else>{{ scope.row.startTime }}</div>
-            </template>
-          </el-table-column>
-          <el-table-column align="center" label="下班时间" width="200" show-overflow-tooltip>
-            <template #default="scope">
-              <div v-if="scope.row.index === workingTimeInfo.clickRow && scope.column.index === workingTimeInfo.clickCell">
-                <el-input
-                  v-model="scope.row.endTime"
-                  maxlength="300"
-                  size="small"
-                  @keyup.enter="updateData"
-                  @blur="updateData"
-                />
-              </div>
-              <div v-else>{{ scope.row.endTime }}</div>
-            </template>
-          </el-table-column>
-        </el-table>
+      <el-row>
+        <el-col :span="18">
+          <div class="workTime-table">
+            <el-table
+              :data="workingTimeInfo.workTime"
+              border
+              stripe
+              @cell-dblclick="tabClick"
+              :row-class-name="tableRowClassName"
+              :cell-class-name="tableCellClassName"
+              style="width: 100%"
+            >
+              <el-table-column align="center" label="序号" width="65">
+                <template #default="scope">
+                  {{ scope.row.index + 1 }}
+                </template>
+              </el-table-column>
+              <el-table-column prop="id" align="center" label="工号" width="200" />
+              <el-table-column prop="name" align="center" label="姓名" width="200" />
+              <el-table-column align="center" label="日期" width="200">
+                <template #default="scope">
+                  <div v-if="scope.row.index === workingTimeInfo.clickRow && scope.column.index === workingTimeInfo.clickCell">
+                    <el-input
+                      ref="dateRef"
+                      v-model="scope.row.date"
+                      maxlength="300"
+                      size="small"
+                      @keyup.enter="updateData"
+                      @blur="updateData"
+                    />
+                  </div>
+                  <div v-else>{{ scope.row.date }}</div>
+                </template>
+              </el-table-column>
+              <el-table-column align="center" label="上班时间" width="200">
+                <template #default="scope">
+                  <div v-if="scope.row.index === workingTimeInfo.clickRow && scope.column.index === workingTimeInfo.clickCell">
+                    <el-input
+                      ref="startTimeRef"
+                      v-model="scope.row.startTime"
+                      maxlength="300"
+                      size="small"
+                      @keyup.enter="updateData"
+                      @blur="updateData"
+                    />
+                  </div>
+                  <div v-else>{{ scope.row.startTime }}</div>
+                </template>
+              </el-table-column>
+              <el-table-column align="center" label="下班时间" width="200" show-overflow-tooltip>
+                <template #default="scope">
+                  <div v-if="scope.row.index === workingTimeInfo.clickRow && scope.column.index === workingTimeInfo.clickCell">
+                    <el-input
+                      ref="endTimeRef"
+                      v-model="scope.row.endTime"
+                      maxlength="300"
+                      size="small"
+                      @keyup.enter="updateData"
+                      @blur="updateData"
+                    />
+                  </div>
+                  <div v-else>{{ scope.row.endTime }}</div>
+                </template>
+              </el-table-column>
+            </el-table>
 
-        <!-- 重置表格 -->
-        <el-button
-          style="position:absolute;left:11px;top:118px;z-index:2;"
-          type="primary"
-          v-show="workingTimeInfo.workTime.length !== 0"
-          :icon="UploadFilled"
-          circle
-          @click="workingTimeInfo.workTime.splice(0)"
-        />
-
-        <!-- 添加数据 -->
-        <el-button
-          style="position:absolute;left:0px;top:160px;z-index:2;"
-          type="primary"
-          :icon="CirclePlusFilled"
-          circle
-          @click="addWorkTimeData()"
-        />
-
-        <el-upload
-          v-show="workingTimeInfo.workTime.length === 0"
-          class="upload-demo"
-          drag
-          action="https://run.mocky.io/v3/9d059bf9-4660-45f2-925d-ce80ad6c4d15"
-          multiple
-          :on-change="handleChange"
-        >
-          <el-icon class="el-icon--upload"><upload-filled /></el-icon>
-          <div class="el-upload__text">
-            <h3>将文件拖拽到此处或点击图标上传文件</h3>
+            <el-upload
+              v-show="workingTimeInfo.workTime.length === 0"
+              class="upload-demo"
+              drag
+              action="https://run.mocky.io/v3/9d059bf9-4660-45f2-925d-ce80ad6c4d15"
+              multiple
+              :on-change="handleChange"
+            >
+              <el-icon class="el-icon--upload"><upload-filled /></el-icon>
+              <div class="el-upload__text">
+                <h3>将文件拖拽到此处或点击图标上传文件</h3>
+              </div>
+              <template #tip>
+                <div class="el-upload__tip" style="text-align: center;">
+                  <h3>请上传文本文件！</h3>
+                </div>
+              </template>
+            </el-upload>
           </div>
-          <template #tip>
-            <div class="el-upload__tip" style="text-align: center;">
-              <h3>请上传文本文件！</h3>
+        </el-col>
+
+        <el-col :span="6">
+          <div class="workTime-use">
+            <div class="function">
+              <h1>功能模块</h1>
+              <el-tooltip  class="box-item"  effect="dark" content="重新读取文本" placement="top">
+                <el-button type="primary" :icon="UploadFilled" circle @click="resumeWorkTimeData()"/>
+              </el-tooltip>
+
+              <el-tooltip  class="box-item"  effect="dark" content="保存本地数据" placement="top">
+                <el-button type="primary" :icon="FolderChecked" circle @click="saveWorkTimeFromLocal()"/>
+              </el-tooltip>
+
+              <el-tooltip  class="box-item"  effect="dark" content="读取本地数据" placement="top">
+                <el-button type="primary" :icon="FolderOpened" circle @click="readWorkTimeFromLocal()"/>
+              </el-tooltip>
+
+              <el-tooltip  class="box-item"  effect="dark" content="增加一行数据" placement="top">
+                <el-button type="primary" :icon="Plus" circle @click="addWorkTimeData()"/>
+              </el-tooltip>
+
+              <el-tooltip  class="box-item"  effect="dark" content="减少一行数据" placement="top">
+                <el-button type="primary" :icon="Minus" circle @click="minusWorkTimeData()"/>
+              </el-tooltip>
+
+              <el-tooltip  class="box-item"  effect="dark" content="计算工时" placement="top">
+                <el-button type="primary" :icon="HelpFilled" circle @click="calWorkTime()"/>
+              </el-tooltip>
             </div>
-          </template>
-        </el-upload>
-      </div>
 
-      <div class="workTime-use">
-        <div class="function">
-          <h1>功能模块</h1>
-          <el-button type="primary" @click="saveWorkTimeFromLocal" style="margin-left: 10px;">保存本地数据</el-button>
-          <el-button type="primary" @click="readWorkTimeFromLocal" style="margin-left: 10px;">读取本地数据</el-button>
-          <el-button type="primary" @click="calWorkTime" style="margin-left: 10px;">计算工时</el-button>
-        </div>
+            <div class="info">
+              <h1>工时统计</h1>
+              当月工作时长：<span>{{ workingTimeInfo.workTime.length }}</span> 天
+              <br>
+              总工时：<span>{{ workingTimeInfo.totalWorkTime}}</span> 时
+              <br>
+              日平均工时： <span>{{ workingTimeInfo.dailyWorkTime }}</span> 时
+              <br>
+            </div>
 
-        <div class="info">
-          <h1>工时统计</h1>
-          当月工作时长：<span>{{ workingTimeInfo.workTime.length }}</span> 天
-          <br>
-          总工时：<span>{{ workingTimeInfo.totalWorkTime}}</span> 时
-          <br>
-          日平均工时： <span>{{ workingTimeInfo.dailyWorkTime }}</span> 时
-          <br>
-        </div>
+            <div class="info" v-if="workingTimeInfo.isCalWorkTime">
+              <h3 v-if="parseFloat(workingTimeInfo.dailyWorkTime) >= 8.5" style="color: green;">您的工时已达到公司要求 请再接再厉！</h3>
+              <h3 v-else style="color: red;">您的工时未达到公司要求 请及时调整！</h3>
+            </div>
 
-        <div class="info" v-if="workingTimeInfo.isCalWorkTime">
-          <h3 v-if="parseFloat(workingTimeInfo.dailyWorkTime) >= 8.5" style="color: green;">您的工时已达到公司要求 请再接再厉！</h3>
-          <h3 v-else style="color: red;">您的工时未达到公司要求 请及时调整！</h3>
-        </div>
-
-        <div class="guide">
-          <h1 style="text-align: center">使用说明</h1>
-          <div style="margin-left: 10px;">
-            <h5>1.在OA系统中，将需要计算的工时复制到txt文本中；(参考src/assets/test.txt格式)</h5>
-            <h5>2.点击表格区域的上传文件按钮打开工时文件，或将工时文件拖拽至目标区域；</h5>
-            <h5>3.将有上下班未打卡的时间补充完整(双击单元格更改)；</h5>
-            <h5>4.点击计算工时按钮，查看工时统计信息。</h5>
+            <div class="guide">
+              <h1 style="text-align: center">使用说明</h1>
+              <div style="margin-left: 10px;">
+                <h5>1.在OA系统中，将需要计算的工时复制到txt文本中；(参考src/assets/test.txt格式)</h5>
+                <h5>2.点击表格区域的上传文件按钮打开工时文件，或将工时文件拖拽至目标区域；</h5>
+                <h5>3.将有上下班未打卡的时间补充完整(双击单元格更改)；</h5>
+                <h5>4.点击计算工时按钮，查看工时统计信息。</h5>
+              </div>
+            </div>
           </div>
-        </div>
-      </div>
+        </el-col>
+      </el-row>
     </div>
   </div>
 </template>
@@ -148,10 +157,12 @@ import { reactive, ref, nextTick } from 'vue'
 
 import { ElMessage } from 'element-plus'
 import type { TableColumnCtx } from 'element-plus/es/components/table/src/table-column/defaults'
-import { UploadFilled, CirclePlusFilled } from '@element-plus/icons-vue'
+import { UploadFilled, FolderChecked, FolderOpened, Plus, Minus, HelpFilled } from '@element-plus/icons-vue'
 import type { UploadProps, UploadFile } from 'element-plus'
 
 const dateRef = ref()
+const startTimeRef = ref()
+const endTimeRef = ref()
 
 interface WorkTimeIt {
   index: number,
@@ -217,9 +228,18 @@ const readWordTimeData = (workTimeRawDataList:Array<string>) => {
         })
       }
     }
+    ElMessage.success('读取数据成功！')
   } catch (error) {
     ElMessage.error('无法正确解析该txt文件')
   }
+}
+
+// 重新读取数据
+const resumeWorkTimeData = () => {
+  workingTimeInfo.workTime.splice(0)
+  workingTimeInfo.totalWorkTime = '0'
+  workingTimeInfo.dailyWorkTime = '0'
+  workingTimeInfo.isCalWorkTime = false
 }
 
 // 增加工时数据
@@ -243,10 +263,20 @@ const addWorkTimeData = () => {
   })
 }
 
+// 减少工时数据
+const minusWorkTimeData = () => {
+  if (workingTimeInfo.workTime.length === 0) {
+    ElMessage.error('没有工时数据！')
+    return
+  }
+  workingTimeInfo.workTime.pop()
+  ElMessage.success('减少最后一个数据成功！')
+}
+
 // 计算工时
 const calWorkTime = () => {
   if (workingTimeInfo.workTime.length === 0) {
-    ElMessage.error('请先读取工时文件！')
+    ElMessage.error('没有工时数据！')
     return
   }
 
@@ -254,8 +284,23 @@ const calWorkTime = () => {
 
   for (let i = 0; i < workingTimeInfo.workTime.length; i++) {
     // 判断是否有未完成的上下班打卡
-    if (workingTimeInfo.workTime[i].startTime === ' ' || workingTimeInfo.workTime[i].endTime === ' ') {
-      ElMessage.error('第' + (i + 1).toString() + '列有未完成的上下班打卡 请先双击表格填写完整！')
+    if (workingTimeInfo.workTime[i].startTime === ' ') {
+      ElMessage.error('第' + (i + 1).toString() + '列有未完成的上班打卡 请先双击表格填写完整！')
+      workingTimeInfo.clickRow = i
+      workingTimeInfo.clickCell = 4
+      workingTimeInfo.workTime[i].startTime = '08:30:00'
+      nextTick(() => {
+        startTimeRef.value.input.focus()
+      })
+      return
+    } else if (workingTimeInfo.workTime[i].endTime === ' ') {
+      ElMessage.error('第' + (i + 1).toString() + '列有未完成的下班打卡 请先双击表格填写完整！')
+      workingTimeInfo.clickRow = i
+      workingTimeInfo.clickCell = 5
+      workingTimeInfo.workTime[i].endTime = '17:00:00'
+      nextTick(() => {
+        endTimeRef.value.input.focus()
+      })
       return
     } else {
       const startTime = new Date(workingTimeInfo.workTime[i].date + ' ' + workingTimeInfo.workTime[i].startTime)
@@ -309,17 +354,29 @@ const updateData = () => {
 
 const saveWorkTimeFromLocal = () => {
   saveWorkTimeData = ''
+
+  if (workingTimeInfo.workTime.length === 0) {
+    ElMessage.error('没有工时数据！')
+    return
+  }
   for (let i = 0; i < workingTimeInfo.workTime.length; i++) {
     saveWorkTimeData += workingTimeInfo.workTime[i].index + '\t' + workingTimeInfo.workTime[i].id + '\t' +
     workingTimeInfo.workTime[i].name + '\t' + workingTimeInfo.workTime[i].date + '\t' + workingTimeInfo.workTime[i].startTime + '\t' +
     workingTimeInfo.workTime[i].endTime + '\n'
   }
   localStorage.setItem('workTimeData', saveWorkTimeData)
+  ElMessage.success('保存数据成功！')
 }
 
 const readWorkTimeFromLocal = () => {
   workingTimeInfo.workTime.splice(0)
-  const workTimeData = localStorage.getItem('workTimeData') as string
+
+  if (localStorage.getItem('workTimeData') === null) {
+    ElMessage.error('没有工时数据！')
+    return
+  }
+
+  const workTimeData = localStorage.getItem('workTimeData')
   const workTimeRawDataList = workTimeData.split('\n')
   readWordTimeData(workTimeRawDataList)
 }
@@ -332,21 +389,14 @@ const readWorkTimeFromLocal = () => {
   }
 
   .workTime-div {
-    margin-top: 50px;
-
-    display: grid;
-    grid-template-columns: repeat(2, 1fr);
-    grid-template-rows: 1fr;
-    grid-column-gap: 0px;
-    grid-row-gap: 0px;
+    margin-top: 30px;
 
     .workTime-table {
       border: 2px solid #008B93;
     }
 
     .workTime-use {
-      height: 550px;
-      width: 520px;
+      // height: 550px;
       margin-left: 10px;
       border: 2px solid #008B93;
 
